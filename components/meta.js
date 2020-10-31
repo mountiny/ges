9b3e28cff12cd96d2991c7f38ab08864a55ec1a0
Vit Horacek <mikllhor@gmail.com>
2020-10-31 13:46:05 +0000

    Favicon routing fixed

--- components/meta.js
+++ components/meta.js
@@ -4,11 +4,11 @@ import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
 export default function Meta() {
   return (
     <Head>
-      <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="manifest" href="/site.webmanifest" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000c70" />
+      <link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
+      <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#000c70" />
       <meta name="msapplication-TileColor" content="#000c70"></meta>
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#ffffff"></meta>
